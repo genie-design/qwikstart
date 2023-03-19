97d8888347ec9cf162540f6416f79c3e58899c17
Corey Jepperson <corey@entropy.cc>
2023-03-19 11:27:19 -0500

    upgrade packages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ export default component$(() => {
 
       <blockquote text="2xl center slate-900" font="semibold" class="italic">
         When you look
-        <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block before:content-[]">
+        <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block before:content-empty">
           <span class="relative text-white">annoyed</span>
         </span>
         all the time, people think that you're busy.
